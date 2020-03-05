e34a1a20a17666ebae33dd98cc7cc53006cdc858
Hayden Daly <hdaly1@stevens.edu>
2020-03-05 13:52:26 -0500

    changing file structure

--- private.js
+++
@@ -1,4 +0,0 @@
-module.exports.USERS_URI = 'mongodb+srv://user123:pass123@cluster0-h0zup.mongodb.net/Users?retryWrites=true&w=majority';
-module.exports.BOOKS_URI = 'mongodb+srv://user123:pass123@cluster0-h0zup.mongodb.net/Books?retryWrites=true&w=majority';
-module.exports.CLUBS_URI = 'mongodb+srv://user123:pass123@cluster0-h0zup.mongodb.net/Clubs?retryWrites=true&w=majority';
-module.exports.GOODREADS_KEY = 'wlmvAWuYsBck0NdjPHGg';
