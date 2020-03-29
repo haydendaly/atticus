ab8393740c97bed556f319bf0c10a4bd9c75acf1
podkolzinmir <podkolzinmiriam@gmail.com>
2020-03-29 15:23:24 -0400

    Added a bookView and clubView screen, updated homeStack

--- frontend/routes/homeStack.js
+++ frontend/routes/homeStack.js
@@ -2,6 +2,10 @@ import { createStackNavigator } from 'react-navigation-stack';
 import { createAppContainer } from 'react-navigation';
 import React from 'react';
 import HomeScreen from '../screens/homeScreen';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import { Button, TouchableOpacity } from 'react-native';
+import ClubView from '../screens/clubView';
+import BookView from'../screens/bookView';
 
 const screens = {
   HomeScreen: {
@@ -11,9 +15,49 @@ const screens = {
         headerShown: true
       };
     }
-  }
+  },
+  ClubView: {
+    screen: ClubView,
+    navigationOptions: ({ navigation }) => {
+      return {
+        headerShown: true,
+        title: 'Club View',
+        headerStyle: { backgroundColor: '#20639B' },
+        headerTitleStyle: { color: '#FFF', fontSize: 24, fontWeight: '500'},
+        headerLeft: () => (
+          <TouchableOpacity onPress={() => navigation.navigate('HomeScreen')} style={{paddingLeft: 8}}>
+            <Icon
+              name='chevron-left'
+              color='#FFF'
+              size={32}
+            />
+          </TouchableOpacity>
+          ),
+      }
+    }
+  },
+  BookView: {
+    screen: BookView,
+    navigationOptions: ({ navigation }) => {
+      return {
+        headerShown: true,
+        title: 'Book View',
+        headerStyle: { backgroundColor: '#20639B' },
+        headerTitleStyle: { color: '#FFF', fontSize: 24, fontWeight: '500'},
+        headerLeft: () => (
+          <TouchableOpacity onPress={() => navigation.navigate('HomeScreen')} style={{paddingLeft: 8}}>
+            <Icon
+              name='chevron-left'
+              color='#FFF'
+              size={32}
+            />
+          </TouchableOpacity>
+          ),
+      }
+    }
+  }  
 };
 
 const HomeStack = createStackNavigator(screens);
-export default HomeStack;
-// export default createAppContainer(HomeStack);
+// export default HomeStack;
+export default createAppContainer(HomeStack);
--- frontend/screens/homeScreen.js
+++ frontend/screens/homeScreen.js
@@ -4,9 +4,10 @@ By: Bruno, Hayden, Madeline, Miriam, and Scott
 #################################################*/
 
 import React, { useState } from "react";
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet,TouchableOpacity,
+  TextInput, Button } from "react-native";
 
-export default function GetStarted({ navigation }) {
+export default function HomeScreen({ navigation }) {
 
   return (
     <View style={styles.container}>
@@ -14,18 +15,69 @@ export default function GetStarted({ navigation }) {
       <Text>Your Clubs</Text>
       <Text>Our Top Picks</Text>
       <Text>Community Favorites</Text>
+    
+   
+   <TouchableOpacity
+      style={styles.clubViewButton}
+      activeOpacity={0.75}
+      onPress={() => navigation.navigate('ClubView')}
+    >
+      <View>
+        <Text style={styles.buttonText}>Go to Club View</Text>
+      </View>
+    </TouchableOpacity>
+    
+
+    <TouchableOpacity
+      style={styles.bookViewButton}
+      activeOpacity={0.70}
+      onPress={() => navigation.navigate('BookView')}
+    >
+      <View>
+        <Text style={styles.buttonText}>Go to Book View</Text>
+      </View>
+    </TouchableOpacity>
+
+
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex: 10,
     backgroundColor: "#eee",
     alignItems: "center"
   },
   text: {
     marginTop: 50,
     fontSize: 32
+  },
+  clubViewButton: {
+    backgroundColor: '#5f9ea0',
+    position: 'absolute',
+    bottom: 110,
+    width: '65%',
+    height: 50,
+    padding: 10,
+    justifyContent: 'space-around',
+    borderRadius: 8,
+    flexDirection: 'row'
+  },
+  bookViewButton:{
+    backgroundColor: '#5f9ea0',
+    position: 'absolute',
+    bottom: 190,
+    width: '65%',
+    height: 50,
+    padding: 10,
+    justifyContent: 'space-around',
+    borderRadius: 8,
+    flexDirection: 'row'
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 19,
+    fontWeight: '500'
   }
 });
