a6f17003f6c7b641c7e3a3e396b151645d339d79
Bruno Salgado <bsalgado@stevens.edu>
2020-03-30 03:01:08 -0400

    Completed club view with placeholder data

--- frontend/screens/clubView.js
+++ frontend/screens/clubView.js
@@ -3,21 +3,101 @@ For: SSW 322
 By: Bruno, Hayden, Madeleine, Miriam, and Scott
 #################################################*/
 
-import React, { useState } from "react";
-import { Text, View, StyleSheet, TouchableOpacity,
-    TextInput, Button } from "react-native";
+import React, { useState } from "react"
+import {
+  Text,
+  View,
+  StyleSheet,
+  TouchableOpacity,
+  TextInput,
+  Button,
+  Image
+} from "react-native"
 
 export default function ClubView({ navigation }) {
-
+  const placeholderPageCountProgress = 462
+  const placeholderPageCountTotal = 782
+  const friends = [
+    {
+      name: "Scott",
+      progress: 309
+    },
+    {
+      name: "Madeleine",
+      progress: 111
+    },
+    {
+      name: "Miriam",
+      progress: 634
+    }
+  ]
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Book Club</Text>
-      <Text>This is clubview</Text>
-
+      <Image
+        source={{
+          uri:
+            "https://images-na.ssl-images-amazon.com/images/I/81VStYnDGrL.jpg"
+        }}
+        style={styles.bookCover}
+      ></Image>
+      <Text style={styles.bookTitle}>Book Title Placeholder</Text>
+      <Text style={styles.bookAuthor}>Book Author Placeholder</Text>
+      <View style={styles.progressBarContainer}>
+        <View style={{ ...styles.progressBarOutline, height: 30 }}></View>
+        <View
+          style={{
+            ...styles.progressBarProgress,
+            width: `${(placeholderPageCountProgress /
+              placeholderPageCountTotal) *
+              100}%`,
+            height: 30
+          }}
+        ></View>
+      </View>
+      <View style={styles.pageCountContainer}>
+        <TextInput
+          style={styles.pageCountInput}
+          value={placeholderPageCountProgress.toString()}
+        ></TextInput>
+        <Text style={styles.pageCountTotal}>
+          {" "}
+          / {placeholderPageCountTotal}
+        </Text>
+      </View>
+      <Text style={styles.text}>
+        {encouragements[Math.floor(Math.random() * encouragements.length)]}
+      </Text>
+      <View style={styles.connectorBall}></View>
+      <View style={styles.connectorBar}></View>
+      <View style={styles.connectorBall}></View>
+      <Text style={styles.text}>Here's how your friends are doing!</Text>
+      <View style={styles.friendContainer}>
+        {friends.map(friend => {
+          return (
+            <View style={styles.friend}>
+              <Text style={styles.friendName}>{friend.name}</Text>
+              <View style={styles.progressBarContainer}>
+                <View
+                  style={{ ...styles.progressBarOutline, height: 10 }}
+                ></View>
+                <View
+                  style={{
+                    ...styles.progressBarProgress,
+                    width: `${(friend.progress / placeholderPageCountTotal) *
+                      100}%`,
+                    height: 10
+                  }}
+                ></View>
+              </View>
+            </View>
+          )
+        })}
+      </View>
     </View>
-
-  );
+  )
 }
+
+const themeColor = "indigo"
 
 const styles = StyleSheet.create({
   container: {
@@ -25,8 +105,90 @@ const styles = StyleSheet.create({
     backgroundColor: "#eee",
     alignItems: "center"
   },
+  bookTitle: {
+    marginTop: 10,
+    fontSize: 24,
+    fontWeight: "bold"
+  },
+  bookAuthor: {
+    fontSize: 16
+  },
   text: {
-    marginTop: 50,
-    fontSize: 32
+    fontSize: 14,
+    margin: 10,
+    color: themeColor
+  },
+  bookCover: {
+    width: "40%",
+    height: "40%",
+    borderRadius: 8,
+    marginTop: 20
+  },
+  progressBarContainer: {
+    width: "80%",
+    marginTop: 10,
+    marginBottom: 10
+  },
+  progressBarOutline: {
+    borderStyle: "solid",
+    borderWidth: 2,
+    borderColor: themeColor,
+    borderRadius: 50,
+    width: "100%"
+  },
+  progressBarProgress: {
+    backgroundColor: themeColor,
+    borderRadius: 50,
+    position: "absolute"
+  },
+  pageCountContainer: {
+    flexDirection: "row"
+  },
+  pageCountInput: {
+    backgroundColor: "white",
+    fontSize: 32,
+    textDecorationLine: "underline",
+    borderColor: themeColor,
+    borderWidth: 2,
+    borderRadius: 8,
+    padding: 5,
+    color: themeColor
+  },
+  pageCountTotal: {
+    fontSize: 32,
+    margin: 5,
+    color: themeColor
+  },
+  connectorBall: {
+    width: 10,
+    height: 10,
+    borderColor: themeColor,
+    borderWidth: 2,
+    borderRadius: 50
+  },
+  connectorBar: {
+    width: 2,
+    height: 50,
+    backgroundColor: themeColor
+  },
+  friendContainer: {
+    flexDirection: "row",
+    width: "100%"
+  },
+  friend: {
+    width: "33%",
+    alignItems: "center"
+  },
+  friendName: {
+    color: themeColor,
+    fontWeight: "bold"
   }
-});
+})
+
+const encouragements = [
+  "Good work!",
+  "Keep going!",
+  "Nice work!",
+  "Nice job!",
+  "Hang in there buddy its almost over I promise"
+]
--- frontend/screens/homeScreen.js
+++ frontend/screens/homeScreen.js
@@ -3,93 +3,112 @@ For: SSW 322
 By: Bruno, Hayden, Madeleine, Miriam, and Scott
 #################################################*/
 
-import React, { useState, useEffect } from "react";
-import books from '../functions/books'
-import users from '../functions/users'
-import { Text, View, FlatList, TouchableOpacity, StyleSheet, SafeAreaView, ScrollView, Image, Dimensions } from "react-native";
+import React, { useState, useEffect } from "react"
+import books from "../functions/books"
+import users from "../functions/users"
+import {
+  Text,
+  View,
+  FlatList,
+  TouchableOpacity,
+  StyleSheet,
+  SafeAreaView,
+  ScrollView,
+  Image,
+  Dimensions
+} from "react-native"
 
 export default class GetStarted extends React.Component {
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
       data: "",
       books: [],
       clubs: []
-    };
-    books.getHomescreen( (books) => {
-      users.getClubs('temp', true, (clubs) => {
+    }
+    books.getHomescreen(books => {
+      users.getClubs("temp", true, clubs => {
         this.setState({
           books: books,
           clubs: clubs
         })
       })
-    });
+    })
   }
-
 
   render() {
     return (
-        <ScrollView showsVerticalScrollIndicator={false}>
-          <View>
-            <Text style={styles.clubText}>Your Clubs</Text>
-            <FlatList
-              horizontal={true}
-              showsHorizontalScrollIndicator={false}
-              data={this.state.clubs}
-              renderItem={({ item }) =>
-                (
-                  <View style={styles.clubHolder1}>
-                    <View style={styles.clubHolder}>
-                      <TouchableOpacity style={styles.clubImage} onPress={() => {
-                        // clubs.getClub(item.clubID, () => {
-                        this.props.navigation.navigate('ClubView')
-                      // })
-                      }}>
-                        <Image source={{ uri: item.imgURL}} style={{width: '100%', height: '100%', borderRadius: 8}}>
-                        </Image>
-                      </TouchableOpacity>
-                      <View style={styles.clubInfo}>
-                        <Text style={styles.clubTitle}>{item.title}</Text>
-                        <Text style={styles.clubAuthor}>By {item.author}</Text>
-                      </View>
-                    </View>
-                  </View>
-                )
-              }
-              keyExtractor={item => item.bookID}
-            />
-          </View>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View>
+          <Text style={styles.clubText}>Your Clubs</Text>
           <FlatList
-            data={this.state.books}
+            horizontal={true}
+            showsHorizontalScrollIndicator={false}
+            data={this.state.clubs}
             renderItem={({ item }) => (
-              <View>
-                <Text style={styles.bookText}>{item.title}</Text>
-                <FlatList
-                  horizontal={true}
-                  showsHorizontalScrollIndicator={false}
-                  data={item.data}
-                  renderItem={({ item }) =>
-                    (
-                      <View style={{paddingLeft: 10}}>
-                        <TouchableOpacity style={styles.book} onPress={() => {
-                          // books.getBook(item.bookID, () => {
-                            this.props.navigation.navigate('BookView')
-                          // })
-                        }}>
-                          <Image source={{ uri: item.imgURL}} style={{width: '100%', height: '100%', borderRadius: 8}}>
-                          </Image>
-                        </TouchableOpacity>
-                      </View>
-                    )
-                  }
-                  keyExtractor={item => item.bookID}
-                />
+              <View style={styles.clubHolder1}>
+                <View style={styles.clubHolder}>
+                  <TouchableOpacity
+                    style={styles.clubImage}
+                    onPress={() => {
+                      // clubs.getClub(item.clubID, () => {
+                      this.props.navigation.navigate("ClubView")
+                      // })
+                    }}
+                  >
+                    <Image
+                      source={{ uri: item.imgURL }}
+                      style={{ width: "100%", height: "100%", borderRadius: 8 }}
+                    ></Image>
+                  </TouchableOpacity>
+                  <View style={styles.clubInfo}>
+                    <Text style={styles.clubTitle}>{item.title}</Text>
+                    <Text style={styles.clubAuthor}>By {item.author}</Text>
+                  </View>
+                </View>
               </View>
             )}
-            keyExtractor={item => item.title}
+            keyExtractor={item => item.bookID}
           />
-        </ScrollView>
-    );
+        </View>
+        <FlatList
+          data={this.state.books}
+          renderItem={({ item }) => (
+            <View>
+              <Text style={styles.bookText}>{item.title}</Text>
+              <FlatList
+                horizontal={true}
+                showsHorizontalScrollIndicator={false}
+                data={item.data}
+                renderItem={({ item }) => (
+                  <View style={{ paddingLeft: 10 }}>
+                    <TouchableOpacity
+                      style={styles.book}
+                      onPress={() => {
+                        // books.getBook(item.bookID, () => {
+                        this.props.navigation.navigate("BookView")
+                        // })
+                      }}
+                    >
+                      <Image
+                        source={{ uri: item.imgURL }}
+                        style={{
+                          width: "100%",
+                          height: "100%",
+                          borderRadius: 8
+                        }}
+                      ></Image>
+                    </TouchableOpacity>
+                  </View>
+                )}
+                keyExtractor={item => item.bookID}
+              />
+            </View>
+          )}
+          keyExtractor={item => item.title}
+        />
+      </ScrollView>
+    )
   }
 }
 
@@ -99,31 +118,31 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff"
   },
   clubText: {
-    marginTop: '4%',
+    marginTop: "4%",
     fontSize: 24,
-    paddingLeft: '4%',
+    paddingLeft: "4%",
     color: "#143e60",
     fontWeight: "500",
-    marginBottom: '1%'
+    marginBottom: "1%"
   },
   bookText: {
     fontSize: 24,
-    paddingLeft: '4%',
+    paddingLeft: "4%",
     color: "#143e60",
     fontWeight: "500",
-    marginTop: '4%',
-    marginBottom: '1%'
+    marginTop: "4%",
+    marginBottom: "1%"
   },
   clubHolder1: {
-    paddingLeft: Dimensions.get('screen').width * .025,
+    paddingLeft: Dimensions.get("screen").width * 0.025
   },
   clubHolder: {
-    width: Dimensions.get('screen').width * .9,
+    width: Dimensions.get("screen").width * 0.9,
     height: 185,
     backgroundColor: "#155149",
     borderRadius: 8,
-    flexDirection: 'row',
-    justifyContent: 'space-between'
+    flexDirection: "row",
+    justifyContent: "space-between"
   },
   clubImage: {
     width: 113.33,
@@ -135,10 +154,10 @@ const styles = StyleSheet.create({
   book: {
     width: 100,
     height: 150,
-    borderRadius: 7,
+    borderRadius: 7
   },
   clubInfo: {
-    width: Dimensions.get('screen').width*.9 - 120,
+    width: Dimensions.get("screen").width * 0.9 - 120
   },
   clubTitle: {
     fontSize: 24,
@@ -146,8 +165,8 @@ const styles = StyleSheet.create({
     marginTop: 10
   },
   clubAuthor: {
-    justifyContent: 'space-between',
+    justifyContent: "space-between",
     marginTop: 5,
     color: "#bbb"
-  },
-});
+  }
+})
