338cc1d708bfff5d7442fb5d353b768499f0f22d
Bruno Salgado <bsalgado@stevens.edu>
2020-04-20 21:35:04 -0400

    Converted clubView.js from class component to functional component

--- frontend/screens/clubView.js
+++ frontend/screens/clubView.js
@@ -15,109 +15,91 @@ import {
     FlatList,
 } from "react-native"
 
-export default class ClubView extends React.Component {
-    constructor(props) {
-        super(props)
-        const club = this.props.navigation.state.params[0]
-        this.state = {
-            currentUserProgress: this.props.navigation.state.params[4],
-            pageTotal: 69,
-            friends: this.props.navigation.state.params[2],
-            book: this.props.navigation.state.params[1],
-            userID: this.props.navigation.state.params[3],
-        }
-    }
+function ClubView(props) {
+    const club = props.navigation.state.params[0]
+    const currentUserProgress = props.navigation.state.params[4]
+    const pageTotal = 100
+    const friends = props.navigation.state.params[2]
+    const book = props.navigation.state.params[1]
+    const userID = props.navigation.state.params[3]
 
-    render() {
-        return (
-            <View style={styles.container}>
-                <Image
-                    source={{
-                        uri: this.state.book.imgURL,
+    return (
+        <View style={styles.container}>
+            <Image
+                source={{
+                    uri: book.imgURL,
+                }}
+                style={styles.bookCover}
+            ></Image>
+            <Text style={styles.bookTitle}>{book.title}</Text>
+            <Text style={styles.bookAuthor}>{book.author}</Text>
+            <View style={styles.progressBarContainer}>
+                <View
+                    style={{ ...styles.progressBarOutline, height: 30 }}
+                ></View>
+                <View
+                    style={{
+                        ...styles.progressBarProgress,
+                        width: `${(currentUserProgress / pageTotal) * 100}%`,
+                        height: 30,
                     }}
-                    style={styles.bookCover}
-                ></Image>
-                <Text style={styles.bookTitle}>{this.state.book.title}</Text>
-                <Text style={styles.bookAuthor}>{this.state.book.author}</Text>
-                <View style={styles.progressBarContainer}>
-                    <View
-                        style={{ ...styles.progressBarOutline, height: 30 }}
-                    ></View>
-                    <View
-                        style={{
-                            ...styles.progressBarProgress,
-                            width: `${
-                                (this.state.currentUserProgress /
-                                    this.state.pageTotal) *
-                                100
-                            }%`,
-                            height: 30,
-                        }}
-                    ></View>
-                </View>
-                <View style={styles.pageCountContainer}>
-                    <TextInput
-                        style={styles.pageCountInput}
-                        value={this.state.currentUserProgress.toString()}
-                    ></TextInput>
-                    <Text style={styles.pageCountTotal}>
-                        {" "}
-                        / {this.state.pageTotal}
-                    </Text>
-                </View>
-                <Text style={styles.text}>
-                    {
-                        encouragements[
-                            Math.floor(Math.random() * encouragements.length)
-                        ]
-                    }
-                </Text>
-                <View style={styles.connectorBall}></View>
-                <View style={styles.connectorBar}></View>
-                <View style={styles.connectorBall}></View>
-                <Text style={styles.text}>
-                    Here's how your friends are doing!
-                </Text>
-                <View style={styles.friendContainer}>
-                    <FlatList
-                        horizontal={true}
-                        showsHorizontalScrollIndicator={false}
-                        contentContainerStyle={{
-                            flex: 1 / this.state.friends.length,
-                        }}
-                        data={this.state.friends}
-                        renderItem={({ item }) => (
-                            <View style={styles.friend}>
-                                <Text style={styles.friendName}>
-                                    {item.userID}
-                                </Text>
-                                <View style={styles.progressBarContainer}>
-                                    <View
-                                        style={{
-                                            ...styles.progressBarOutline,
-                                            height: 10,
-                                        }}
-                                    ></View>
-                                    <View
-                                        style={{
-                                            ...styles.progressBarProgress,
-                                            width: `${
-                                                (item.progress /
-                                                    this.state.pageCountTotal) *
-                                                100
-                                            }%`,
-                                            height: 10,
-                                        }}
-                                    ></View>
-                                </View>
-                            </View>
-                        )}
-                        keyExtractor={(item, index) => "key" + index}
-                    />
-                </View>
+                ></View>
             </View>
-        )
-    }
+            <View style={styles.pageCountContainer}>
+                <TextInput
+                    style={styles.pageCountInput}
+                    onChangeText={(text) => console.warn(text)}
+                ></TextInput>
+                <Text style={styles.pageCountTotal}> / {pageTotal}</Text>
+            </View>
+            <Text style={styles.text}>
+                {
+                    encouragements[
+                        Math.floor(Math.random() * encouragements.length)
+                    ]
+                }
+            </Text>
+            <View style={styles.connectorBall}></View>
+            <View style={styles.connectorBar}></View>
+            <View style={styles.connectorBall}></View>
+            <Text style={styles.text}>Here's how your friends are pooing!</Text>
+            <View style={styles.friendContainer}>
+                <FlatList
+                    horizontal={true}
+                    showsHorizontalScrollIndicator={false}
+                    contentContainerStyle={{
+                        flex: 1 / friends.length,
+                    }}
+                    data={friends}
+                    renderItem={({ item }) => (
+                        <View style={styles.friend}>
+                            <Text style={styles.friendName}>{item.userID}</Text>
+                            <View style={styles.progressBarContainer}>
+                                <View
+                                    style={{
+                                        ...styles.progressBarOutline,
+                                        height: 10,
+                                    }}
+                                ></View>
+                                <View
+                                    style={{
+                                        ...styles.progressBarProgress,
+                                        width: `${
+                                            (item.progress /
+                                                styles.pageCountTotal) *
+                                            100
+                                        }%`,
+                                        height: 10,
+                                    }}
+                                ></View>
+                            </View>
+                        </View>
+                    )}
+                    keyExtractor={(item, index) => "key" + index}
+                />
+            </View>
+        </View>
+    )
 }
 
 const themeColor = "indigo"
@@ -214,3 +196,4 @@ const encouragements = [
     "Nice job!",
     "Hang in there buddy its almost over I promise",
 ]
+export default ClubView
